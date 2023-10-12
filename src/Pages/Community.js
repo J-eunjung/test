a34633cf13e009755e87afaf5e18115475090a78
ej <j63595809@gmail.com>
2023-10-12 20:37:45 +0900

    3

--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -21,7 +21,7 @@ function Community() {
       </div>
       <div className="rank">
         <div className="title">
-          <p>인기 상승 그룹</p>
+          <p>인기 상승 그룹111111111</p>
         </div>
         <PopularRank />
       </div>
